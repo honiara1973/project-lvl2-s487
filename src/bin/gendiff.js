d32ba56409a96d8ead08ff5b4dadb741910445a7
Anna <honiara1951@gmail.com>
2019-05-29 14:57:55 +0300

    Выполнила задание 1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('Hello World!');
+const program = require('commander');
+
+program
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format');
+
+program
+  .description('Compares two configuration files and shows a difference.');
+
+program
+  .arguments('<firstconfig> <secondconfig>');
+
+program.parse(process.argv);
+
+program.help();
