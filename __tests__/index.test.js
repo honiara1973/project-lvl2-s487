a4fdc0e524f8eef7bccb4d6e9d2b8136fd7dd6bc
Anna <honiara1951@gmail.com>
2019-06-17 15:53:17 +0300

    Закончила задание 5: сделала ini и тесты

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,65 @@
 import genDiff from '../src';
 
-const beforeJson = './__tests__/__fixtures__/before.json';
-const afterJson = './__tests__/__fixtures__/after.json';
+const beforeJson = './__tests__/__fixtures__/plain/before.json';
+const afterJson = './__tests__/__fixtures__/plain/after.json';
 
-const beforeYaml = './__tests__/__fixtures__/before.yml';
-const afterYaml = './__tests__/__fixtures__/after.yml';
+const beforeYaml = './__tests__/__fixtures__/plain/before.yml';
+const afterYaml = './__tests__/__fixtures__/plain/after.yml';
 
-const beforeIni = './__tests__/__fixtures__/before.ini';
-const afterIni = './__tests__/__fixtures__/after.ini';
+const beforeIni = './__tests__/__fixtures__/plain/before.ini';
+const afterIni = './__tests__/__fixtures__/plain/after.ini';
+
+const beforeJsonNested = './__tests__/__fixtures__/nested/before.json';
+const afterJsonNested = './__tests__/__fixtures__/nested/after.json';
+
+const beforeYamlNested = './__tests__/__fixtures__/nested/before.yml';
+const afterYamlNested = './__tests__/__fixtures__/nested/after.yml';
+
+const beforeIniNested = './__tests__/__fixtures__/nested/before.ini';
+const afterIniNested = './__tests__/__fixtures__/nested/after.ini';
 
 const result = `{
-  host: hexlet.io
-- timeout: 50
-+ timeout: 20
-- proxy: 123.234.53.22
-- follow: false
-+ verbose: true
+    host: hexlet.io
+  - timeout: 50
+  + timeout: 20
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
+
+const resultNested = `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+        setting6: {
+            key: value
+          + ops: vops
+        }
+      + follow: false
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
 }`;
 
 test('compare JSON', () => {
@@ -28,4 +72,16 @@ test('compare Yaml', () => {
 
 test('compare Ini', () => {
   expect(genDiff(beforeIni, afterIni)).toBe(result);
+});
+
+test('compare nestedJSON', () => {
+  expect(genDiff(beforeJsonNested, afterJsonNested)).toBe(resultNested);
+});
+
+test('compare nestedYaml', () => {
+  expect(genDiff(beforeYamlNested, afterYamlNested)).toBe(resultNested);
+});
+
+test('compare nestedIni', () => {
+  expect(genDiff(beforeIniNested, afterIniNested)).toBe(resultNested);
 });
