a4c62463f6c37c54adf3d59a644908340b129eec
Anna <honiara1951@gmail.com>
2019-09-28 20:50:38 +0300

    Исправила замечания ментора (блок 5)

--- src/diff.js
+++ src/diff.js
@@ -12,25 +12,36 @@ const processData = (key, type, valueBefore, valueAfter, children = []) => {
   return entry;
 };
 
+const cases = [
+  {
+    check: (key, obj1, obj2) => obj1[key] instanceof Object && obj2[key] instanceof Object,
+    process: (key, obj1, obj2, fn) => processData(key, 'nested', '', '', fn(obj1[key], obj2[key])),
+  },
+  {
+    check: (key, obj1, obj2) => obj1[key] === obj2[key],
+    process: (key, obj1) => processData(key, 'sameValue', obj1[key], ''),
+  },
+  {
+    check: (key, obj1, obj2) => _.has(obj1, key) && _.has(obj2, key),
+    process: (key, obj1, obj2) => processData(key, 'changedValue', obj1[key], obj2[key]),
+  },
+  {
+    check: (key, obj1, obj2) => !_.has(obj1, key) && _.has(obj2, key),
+    process: (key, obj1, obj2) => processData(key, 'added', '', obj2[key]),
+  },
+  {
+    check: (key, obj1, obj2) => _.has(obj1, key) && !_.has(obj2, key),
+    process: (key, obj1) => processData(key, 'deleted', obj1[key], ''),
+  },
+];
+
 const getDifferences = (obj1, obj2) => {
   const allKeys = _.union(_.keys(obj1), _.keys(obj2));
 
-  const differences = allKeys.map((it) => {
-    if (_.has(obj1, it) && _.has(obj2, it)) {
-      if (obj1[it] instanceof Object && obj2[it] instanceof Object) {
-        return processData(it, 'nested', '', '', getDifferences(obj1[it], obj2[it]));
-      }
-
-      return obj1[it] === obj2[it] ? processData(it, 'sameValue', obj1[it], '')
-        : processData(it, 'changedValue', obj1[it], obj2[it]);
-    }
-
-    if (!_.has(obj1, it) && _.has(obj2, it)) {
-      return processData(it, 'added', '', obj2[it]);
-    }
-
-    return processData(it, 'deleted', obj1[it], '');
-  });
+  const differences = (allKeys.map(it => (cases
+    .find(({ check }) => check(it, obj1, obj2))
+    .process(it, obj1, obj2, getDifferences)
+  )));
 
   return differences;
 };
